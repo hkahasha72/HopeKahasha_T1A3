d3dd8842ba759012e047b09ee40126c25f2eb72c
hopekahasha1 <14479@coderacademy.edu.au>
2023-05-12 19:47:03 +1000

    i have added the time.sleep function, in order to allow the user to be able to read the instructions i have implimented that function so that the view has some time to read through the task, what they need to accomplish and what the requirements are

--- main.py
+++ main.py
@@ -36,6 +36,9 @@ def run_driver_test():
     print('Hello, ' + my_name + ' today youll be taking a practice drivers test which will help you prepare for the real thing, you will have 25 minutes to complete this test and must answer at least 26 out of the 32 questions correctly ')
     print("Let's begin!\n")
     
+    # Gives reader time to read the description before the test starts
+    time.sleep(7)
+
     # option which Shuffles the questions
     random.shuffle(questions)
 

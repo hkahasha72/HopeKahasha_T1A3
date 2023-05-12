f95817b748ce71bb1c5c21228cbb42b014ef9729
hopekahasha1 <14479@coderacademy.edu.au>
2023-05-12 19:06:21 +1000

    defined a small sample of  questions which  will be displayed in the app in order to help user answer and practice for their test, instilling a foundation makes it easier to work up

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+
+# This Section is the Defined, questions + answers for the driver's test, plus the hints
+questions = [
+    {
+        'question': 'Question 1 of 32: You are in a crash. The police are not in attendance. Someone is injured. What should you do?',
+        'options': ['A: Make sure the injured person is alright then drive on', 'B: Report the crash to the nearest open police station', 'C: Report the crash to the nearest open police station if property has also been damaged'],
+        'answer': 'B: Report the crash to the nearest open police station',
+        'hint': 'it requires immediate action'
+    },
+    {
+        'question': 'Question 2 of 32: What should you do when a worker on the road in front of you is holding a yellow circular sign that displays the text "Slowly" sign?',
+        'options': ['A: Proceed slowly', 'B: Proceed at the speed limit', 'C: Stop and wait until it is safe to proceed slowly'],
+        'answer': 'A: Proceed slowly',
+        'hint': 'Its implied on the sign'
+    },
+    {
+        'question': 'Question 3 of 32: When is vehicle A allowed to turn right?',
+        'options': ['A: Before vehicle B turns right', 'B: After vehicle B has turned right', 'C: Vehicle A is not allowed to turn right.'],
+        'answer': 'C: Vehicle A is not allowed to turn right.',
+        'hint': 'Using a hands-free system allows you to communicate safely while driving.'
+    },
+]
+
 print( "Welcome to LetsDrive! the VIC Drivers Learner Test Practice app!, Input your name to start")
 my_name = input()
 print('Hello, ' + my_name + ' today youll be taking a practice drivers test which will help you prepare for the real thing, you will have 25 minutes to complete this test and must answer at least 26 out of the 32 questions correctly ')

19ef6ed87093f433634f9b461c70bad81e635010
hopekahasha1 <14479@coderacademy.edu.au>
2023-05-12 18:50:06 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print( "Welcome to LetsDrive! the VIC Drivers Learner Test Practice app!, Input your name to start")
+my_name = input()
+print('Hello, ' + my_name + ' today youll be taking a practice drivers test which will help you prepare for the real thing, you will have 25 minutes to complete this test and must answer at least 26 out of the 32 questions correctly ')
+print("Let's begin!\n")

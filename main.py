cf5aeeea3cf3af20dd8028a9d59845c6f0d26ea7
hopekahasha1 <14479@coderacademy.edu.au>
2023-05-13 22:38:12 +1000

    affter transfering dictionaries to json, i have sucessfully applied it to the main and the code runs smoothly, took a bit of renaming and maintience but it works

--- main.py
+++ main.py
@@ -1,43 +1,15 @@
-import random
 import time
 from colorama import init, Fore, Style
-import json as json
+import json
 
 # Initialize colorama
 init(autoreset=True)
 
-# This Section is the Defined, questions + answers for the driver's test, plus the hints
-data_string = ''
-{
-    "questions": [
-        {
-            "question": "You are in a crash. The police are not in attendance. Someone is injured. What should you do?",
-            "options": ["A: Make sure the injured person is alright then drive on", "B: Report the crash to the nearest open police station", "C: Report the crash to the nearest open police station if property has also been damaged"],
-            "answer": "B",
-            "hint": "it requires immediate action"
-        },
-        {
-            "question": "What should you do when a worker on the road in front of you is holding a yellow circular sign that displays the text Slowly sign?",
-            "options": ["A: Proceed slowly", "B: Proceed at the speed limit", "C: Stop and wait until it is safe to proceed slowly"],
-            "answer": "A",
-            "hint": "Its implied on the sign"
-        },
-        {
-            "question": "When is vehicle A allowed to turn right?",
-            "options": ["A: Before vehicle B turns right", "B: After vehicle B has turned right", "C: Vehicle A is not allowed to turn right."],
-            "answer": "C",
-            "hint": "Using a hands-free system allows you to communicate safely while driving."
-        }
-    ]
-}
-''
-data = json.loads(question_string)
+# allows access to JSON
+with open('test_question.json') as f:
+    test_question = json.load(f)
 
-print(type(data))
-print(data)
-
-
-#Defines 
+# Defines
 def display_question(question):
     print(Fore.YELLOW + question['question'])
     print(Fore.BLUE + "Hint:", question['hint'])
@@ -45,29 +17,27 @@ def display_question(question):
         print(Fore.CYAN + f"{index + 1}. {option}")
     print(Style.RESET_ALL)
 
-# defines 
+# Defines the total number of questions
+def total_questions():
+    return len(test_question['test_question'])
+
+# Defines
 def run_driver_test():
     score = 0
-    total_questions = len(questions)
     incorrect_answers = []
 
-
-    print(Fore.GREEN + "Welcome to LetsDrive! the VIC Drivers Learner Test Practice app!, Input your name to start")
+    print(Fore.GREEN + "Welcome to LetsDrive! the VIC Drivers Learner Test Practice app! Input your name to start")
     my_name = input()
-    print('Hello, ' + my_name + ' today youll be taking a practice drivers test which will help you prepare for the real thing, you will have 25 minutes to complete this test and must answer at least 26 out of the 32 questions correctly ')
+    print('Hello, ' + my_name + ' today you\'ll be taking a practice driver\'s test which will help you prepare for the real thing. You will have 25 minutes to complete this test and must answer at least 26 out of the 32 questions correctly.')
     print("Let's begin!\n")
 
-    # Gives reader time to read the description before the test starts
-    time.sleep(7)
-
-    # Shuffle the questions
-    random.shuffle(questions)
+    # Gives the reader time to read the description before the test starts
+    time.sleep(3)
 
     # Iterate through the questions
-    for question in questions:
-        display_question(question) 
+    for question in test_question['test_question']:
+        display_question(question)
         user_answer = input(Fore.WHITE + "Enter answer A, B, C: ").strip().upper()
-       
 
         # Check the answer
         if user_answer == question['answer'].upper():
@@ -76,15 +46,14 @@ def run_driver_test():
         else:
             incorrect_answers.append(question)
             print(Fore.RED + "Incorrect!")
-           
 
         print()
 
-    # Calculate the passing score
-    passing_score = int(0.8 * total_questions)
+    # Calculates the user's passing score
+    passing_score = int(0.8 * total_questions())
 
-    print(Fore.BLUE + "Test complete " + my_name + "!")
-    print(Fore.CYAN + "You Got A Score Of:", score, "/", total_questions)
+    print(Fore.BLUE + "Test complete, " + my_name + "!")
+    print(Fore.CYAN + "You Got A Score Of:", score, "/", total_questions())
 
     if score >= passing_score:
         print(Fore.GREEN + "Congratulations! You have passed the driver's test!")
@@ -96,4 +65,6 @@ def run_driver_test():
             print(question['question'])
             print("Correct Answer:", question['answer'])
 
+# Runs the test
 run_driver_test()
+
